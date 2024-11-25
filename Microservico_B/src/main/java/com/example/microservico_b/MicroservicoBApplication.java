91045f0c5760f7cbce67ac25a01189e0016c470c
Felipe Tadiello <tadiellofelipe@gmail.com>
2024-11-25 15:44:02 -0300

    initial commit

---
+++ Microservico_B/src/main/java/com/example/microservico_b/MicroservicoBApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservico_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicoBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicoBApplication.class, args);
+    }
+
+}
