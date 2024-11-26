f256d4e776e52e460a7fe5430cbf19ab0f8b4c6f
André Branco <andre_c_branco@estudante.sc.senai.br>
2024-11-26 15:19:25 -0300

    build | created 'Delete' controller and service

--- Microservico_B/src/main/java/com/example/microservico_b/controller/PostController.java
+++ Microservico_B/src/main/java/com/example/microservico_b/controller/PostController.java
@@ -3,10 +3,9 @@ package com.example.microservico_b.controller;
 import com.example.microservico_b.model.entities.Post;
 import com.example.microservico_b.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -14,5 +13,18 @@ public class PostController {
 
     @Autowired
     private PostService postService;
+
+
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletePost(@PathVariable Long id){
+        try {
+            postService.delete(id);
+            return ResponseEntity.ok("Deleted with Success");
+        }catch(Exception ex) {
+            return new ResponseEntity("Query Error", HttpStatusCode.valueOf(504));
+        }
+    }
 
 }
--- Microservico_B/src/main/java/com/example/microservico_b/service/PostService.java
+++ Microservico_B/src/main/java/com/example/microservico_b/service/PostService.java
@@ -3,8 +3,12 @@ package com.example.microservico_b.service;
 import com.example.microservico_b.client.JsonPlaceholderClient;
 import com.example.microservico_b.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import com.example.microservico_b.model.entities.Post;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
 public class PostService {
@@ -17,6 +21,11 @@ public class PostService {
 
     public Post save(Post post){
         return postRepository.save(post);
+    }
+
+    public void delete(Long id) {
+        postRepository.deleteById(id);
+
     }
 
 }
