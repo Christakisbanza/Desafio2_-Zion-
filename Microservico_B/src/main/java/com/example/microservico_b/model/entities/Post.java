453823afbd8049b269d2e4a8eaefc011e4d4619e
Christakis <banzachristakis@gmail.com>
2024-11-26 15:22:55 -0300

    featue/ Add dependence ModelMapper and Update Class Post

--- Microservico_B/src/main/java/com/example/microservico_b/model/entities/Post.java
+++ Microservico_B/src/main/java/com/example/microservico_b/model/entities/Post.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 @Getter
 @Setter
@@ -11,8 +12,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 public class Post {
 
+    @Id
     private Long id;
     private String title;
     private String description;
+    private String body;
 
 }
