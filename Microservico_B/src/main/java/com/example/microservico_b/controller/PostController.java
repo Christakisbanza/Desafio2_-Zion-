cfc42e5ad80bbf09c2f9f521bd78e07725913198
Christakis <banzachristakis@gmail.com>
2024-11-26 15:33:16 -0300

    build | 'Post' on PostController

--- Microservico_B/src/main/java/com/example/microservico_b/controller/PostController.java
+++ Microservico_B/src/main/java/com/example/microservico_b/controller/PostController.java
@@ -1,9 +1,15 @@
 package com.example.microservico_b.controller;
 
+import com.example.microservico_b.controller.dto.PostCreateDto;
+import com.example.microservico_b.controller.dto.PostResponseDto;
+import com.example.microservico_b.controller.mapper.PostMapper;
 import com.example.microservico_b.model.entities.Post;
+import com.example.microservico_b.repository.PostRepository;
 import com.example.microservico_b.service.PostService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +20,11 @@ public class PostController {
 
     @Autowired
     private PostService postService;
+
+    @PostMapping
+    public ResponseEntity<PostResponseDto> create(@RequestBody @Valid PostCreateDto postCreateDto){
+        Post post = postService.save(PostMapper.toPost(postCreateDto));
+        return ResponseEntity.ok().body(PostMapper.toDto(post));
+    }
 
 }
