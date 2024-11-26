1ed312728cd24915167bdfd5795658c889c87d91
Felipe Tadiello <tadiellofelipe@gmail.com>
2024-11-26 13:51:01 -0300

    feat: create class PostService

---
+++ Microservico_B/src/main/java/com/example/microservico_b/service/PostService.java
@@ -0,0 +1,18 @@
+package com.example.microservico_b.service;
+
+import com.example.microservico_b.client.JsonPlaceholderClient;
+import com.example.microservico_b.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.microservico_b.model.entities.Post;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private JsonPlaceholderClient jsonPlaceholderClient;
+
+    @Autowired
+    private PostRepository postRepository;
+
+}
