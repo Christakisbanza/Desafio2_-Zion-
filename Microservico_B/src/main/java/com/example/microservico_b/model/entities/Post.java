3e89ce6c554acd88695f4f2e58408d15c64b2ee3
Felipe Tadiello <tadiellofelipe@gmail.com>
2024-11-27 10:47:41 -0300

    feat: restructure Post entity and DTO

--- Microservico_B/src/main/java/com/example/microservico_b/model/entities/Post.java
+++ Microservico_B/src/main/java/com/example/microservico_b/model/entities/Post.java
@@ -1,21 +1,33 @@
 package com.example.microservico_b.model.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.UniqueElements;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 @Getter
 @Setter
-@NoArgsConstructor @AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @Document
 public class Post {
 
     @Id
+    @JsonIgnore
     private String id;
-    private String title;
-    private String description;
-    private String body;
 
+    @UniqueElements
+    @JsonProperty("id")
+    private Long postId;
+
+    @JsonProperty("userId")
+    private Long userId;
+
+    private String title;
+    private String body;
 }
