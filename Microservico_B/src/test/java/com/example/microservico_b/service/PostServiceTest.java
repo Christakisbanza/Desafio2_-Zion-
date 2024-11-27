3bb946c2ccb93683297214beab14f90b99ec2f04
André Branco <andre_c_branco@estudante.sc.senai.br>
2024-11-27 17:09:35 -0300

    test | delete test in development

--- Microservico_B/src/test/java/com/example/microservico_b/service/PostServiceTest.java
+++ Microservico_B/src/test/java/com/example/microservico_b/service/PostServiceTest.java
@@ -1,20 +1,34 @@
 package com.example.microservico_b.service;
 
+import com.example.microservico_b.client.JsonPlaceholderClient;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 import static com.example.microservico_b.common.PostConstants.POST;
 
 @SpringBootTest(classes = PostService.class)
+@ExtendWith(MockitoExtension.class)
 public class PostServiceTest {
+
+    @Mock
+    private JsonPlaceholderClient jsonPlaceholderClient;
+
+
+    @InjectMocks
     private PostService postService;
 
 
+
     @Test
-    public void deletePost_WithValidId_DeletesPost(){
+    public void deletePost_WithValidId_DoesNotThrowAnyException() {
         postService.deletePost(POST.getId());
 
-
+        assertThat(POST).isNull();
     }
 }
