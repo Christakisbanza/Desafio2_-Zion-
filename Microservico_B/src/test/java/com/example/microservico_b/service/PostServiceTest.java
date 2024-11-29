980779d76f0ce83c34d86f773b7ea57af355a0fd
André Branco <andre_c_branco@estudante.sc.senai.br>
2024-11-29 08:30:15 -0300

    cleanup | delete unit test cleanup

--- Microservico_B/src/test/java/com/example/microservico_b/service/PostServiceTest.java
+++ Microservico_B/src/test/java/com/example/microservico_b/service/PostServiceTest.java
@@ -2,38 +2,25 @@ package com.example.microservico_b.service;
 
 import com.example.microservico_b.client.JsonPlaceholderClient;
 import com.example.microservico_b.exception.PostNotFoundException;
-import com.example.microservico_b.model.entities.Post;
 import com.example.microservico_b.repository.PostRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-
-import java.util.Optional;
-
 import static com.example.microservico_b.common.PostConstants.POST;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest(classes = PostService.class)
 @ExtendWith(MockitoExtension.class)
 public class PostServiceTest {
 
-    @Mock
-    private JsonPlaceholderClient jsonPlaceholderClient;
 
     @MockitoBean
     private PostService postService;
-
-
-    private PostRepository postRepository = mock(PostRepository.class);
-
 
 
     @Test
