67397cfb977f5d9108e1008885ff08ffb6b971cc
André Branco <andre_c_branco@estudante.sc.senai.br>
2024-11-29 08:48:34 -0300

    test | delete service test

--- Microservico_B/src/test/java/com/example/microservico_b/service/PostServiceTest.java
+++ Microservico_B/src/test/java/com/example/microservico_b/service/PostServiceTest.java
@@ -1,40 +1,23 @@
 package com.example.microservico_b.service;
 
-import com.example.microservico_b.client.JsonPlaceholderClient;
 import com.example.microservico_b.exception.PostNotFoundException;
-import com.example.microservico_b.model.entities.Post;
-import com.example.microservico_b.repository.PostRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-
-import java.util.Optional;
-
 import static com.example.microservico_b.common.PostConstants.POST;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest(classes = PostService.class)
 @ExtendWith(MockitoExtension.class)
 public class PostServiceTest {
 
-    @Mock
-    private JsonPlaceholderClient jsonPlaceholderClient;
 
     @MockitoBean
     private PostService postService;
-
-    @Mock
-    private PostRepository postRepository;
-
 
 
     @Test
@@ -44,7 +27,6 @@ public class PostServiceTest {
 
     @Test
     public void deletePost_WithInvalidId_ThrowsException() {
-
         doThrow(new PostNotFoundException("Post with id 9999 not found")).when(postService).delete(9999);
 
         assertThatThrownBy(() -> postService.delete(9999))
