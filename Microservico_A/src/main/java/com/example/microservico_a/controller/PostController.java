0dd09a02f1f69a2124823d20c7b1c792b7f34c03
André Branco <andre_c_branco@estudante.sc.senai.br>
2024-11-29 14:58:42 -0300

    MSA feat | geybyid documentation

--- Microservico_A/src/main/java/com/example/microservico_a/controller/PostController.java
+++ Microservico_A/src/main/java/com/example/microservico_a/controller/PostController.java
@@ -1,6 +1,12 @@
 package com.example.microservico_a.controller;
+import com.example.microservico_a.controller.dto.PostResponseDto;
+import com.example.microservico_a.controller.exception.ErrorMessage;
 import com.example.microservico_a.entities.Post;
 import com.example.microservico_a.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +21,25 @@ public class PostController implements Serializable {
     @Autowired
     private PostService postService;
 
+
+
+
+    @Operation(
+            summary = "Retrieve by Id",
+            description = "Find a Post by Id",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Resource retrieved successfully",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = PostResponseDto.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Resource not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))
+                    )
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<Post> getById(@PathVariable int id) {
         Post post = postService.findById(id);
