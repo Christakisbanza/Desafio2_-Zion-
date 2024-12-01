5950a2c6f16d6df7270f8365feeb7836ab68e727
Felipe Tadiello <tadiellofelipe@gmail.com>
2024-12-01 17:26:23 -0300

    MS-A & MS-B | feat: update PostResponseDto to include fields

--- Microservico_B/src/main/java/com/example/microservico_b/controller/dto/PostResponseDto.java
+++ Microservico_B/src/main/java/com/example/microservico_b/controller/dto/PostResponseDto.java
@@ -8,7 +8,8 @@ import lombok.Setter;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class PostResponseDto {
-    private String id;
+    private Integer id;
+    private Integer userId;
     private String title;
     private String body;
 }
