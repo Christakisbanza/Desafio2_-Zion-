163fd8a9671fb0f972a16a8c9638f798def32340
André Branco <andre_c_branco@estudante.sc.senai.br>
2024-12-02 15:05:26 -0300

    Merga main | MSA-developer_andre/test_getbyid

---
+++ Microservico_A/src/test/java/com/example/microservico_a/common/PostConstants.java
@@ -0,0 +1,7 @@
+package com.example.microservico_a.common;
+
+import com.example.microservico_a.entities.Post;
+
+public class PostConstants {
+    public static final Post POST = new Post(1, 1, "title", "body");
+}
