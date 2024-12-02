fbea9bb0b935d072959a6f3b43df5079890de3a4
André Branco <andre_c_branco@estudante.sc.senai.br>
2024-12-02 15:00:23 -0300

    MSA feat | getById tests

--- Microservico_A/src/test/java/com/example/microservico_a/service/PostServiceTest.java
+++ Microservico_A/src/test/java/com/example/microservico_a/service/PostServiceTest.java
@@ -2,6 +2,8 @@ package com.example.microservico_a.service;
 
 import com.example.microservico_a.client.JsonPlaceholderClient;
 import com.example.microservico_a.entities.Post;
+import com.example.microservico_a.exception.EntityNotFoundException;
+import com.example.microservico_a.exception.PostNotFoundException;
 import com.example.microservico_a.repository.PostRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,12 +12,19 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
+import static com.example.microservico_a.common.PostConstants.POST;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
@@ -59,6 +68,25 @@ public class PostServiceTest {
         assertTrue(result.isEmpty());
         verify(jsonPlaceholderClient, times(1)).getPosts();
     }
+
+    @Test
+    public void getPostById_WithValidId_DoesNotThrowAnyException() {
+        when(jsonPlaceholderClient.findById(POST.getId())).thenReturn(new ResponseEntity<>(POST, HttpStatus.OK));
+
+       Post post = postService.findById(POST.getId());
+
+        assertThat(post).isNotNull();
+        assertThat(post).isEqualTo(POST);
+        verify(jsonPlaceholderClient, times(1)).findById(POST.getId());
+    }
+
+    @Test
+    public void getPostById_WithInvalidId_ReturnsException() {
+        when(jsonPlaceholderClient.findById(POST.getId())).thenReturn(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+
+        assertThrows(PostNotFoundException.class, () -> postService.findById(POST.getId()));
+    }
+
 
 
 
