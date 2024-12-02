f9b6de8a9caa946177d4f52d64b4b24e0249c5e9
padsjr <pedroadasilveirajr@hotmail.com>
2024-12-02 14:58:32 -0300

    MSA-developer /translate to english

--- Microservico_A/src/test/java/com/example/microservico_a/common/PostConstants.java
+++
@@ -1,7 +0,0 @@
-package com.example.microservico_a.common;
-
-import com.example.microservico_a.entities.Post;
-
-public class PostConstants {
-    public static final Post POST = new Post(1, 1, "title", "body");
-}
